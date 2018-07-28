d509fc9caefb51e5f7b0291634720654523ed757
sergey <sergry.ivanovich.sidorov@gmail.com>
2018-07-28 23:31:07 +0300

    add all pages, some content and typed hello message

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 colors = ["#7396D1", "#D3D4D7", "#505F99", "#FFBD25", "#7396D1", "#C33949"];
+roles = ["Sergey Sidorov", "a Developer", "a Software Engineer", "Sergey Sidorov"];
 maxlength = 15;
 function setColor(){
     parts = $("#welcome").text().split('.').filter(function(e){
@@ -13,6 +14,15 @@ function setColor(){
     $("#welcome").html(output);
 }
 
-function getRandomInt(min, max){
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function runTypedAnimation() {
+    if($(".typed p span").length) {
+        $(".typed p span").typed({
+            strings: roles,
+            typeSpeed: 50
+        });
+    }
 }
