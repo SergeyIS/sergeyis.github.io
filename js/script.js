e8b5a5b94e306446cb4eaef5c303c3d66dca9244
sergey <sergry.ivanovich.sidorov@gmail.com>
2018-07-29 19:22:11 +0300

    add Skills, animation of titles

--- js/script.js
+++ js/script.js
@@ -1,22 +1,5 @@
 colors = ["#7396D1", "#D3D4D7", "#505F99", "#FFBD25", "#7396D1", "#C33949"];
-roles = ["Sergey Sidorov", "a Developer", "a Software Engineer", "Sergey Sidorov"];
-maxlength = 15;
-function setColor(){
-    parts = $("#welcome").text().split('.').filter(function(e){
-        return e !== "";
-    });
-    
-    output = "";
-    parts.forEach(function(element) {
-        output += "<p style='display:inline; color:" + colors[getRandomInt(0, colors.length - 1)] + "'>" + element + "." +"</p>"
-    }, this);
-
-    $("#welcome").html(output);
-}
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+roles = [dictionary.my_name, dictionary.a_developer, dictionary.a_software_engineer, dictionary.my_name];
 
 function runTypedAnimation() {
     if($(".typed p span").length) {
