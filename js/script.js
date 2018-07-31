eaa09bc2aa0d2f6583de2e83b2bef8de8b563abc
sergey <sergry.ivanovich.sidorov@gmail.com>
2018-08-01 00:54:53 +0300

    add style files for each device type. work on mobile version

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 colors = ["#7396D1", "#D3D4D7", "#505F99", "#FFBD25", "#7396D1", "#C33949"];
 roles = [dictionary.my_name, dictionary.a_developer, dictionary.a_software_engineer, dictionary.my_name];
 
-function runTypedAnimation() {
-    if($(".typed p span").length) {
-        $(".typed p span").typed({
-            strings: roles,
-            typeSpeed: 50
-        });
+function runTypedAnimation(obj) {
+    if(obj.isRunning === undefined || obj.isRunning === false){
+        obj.isRunning = true;
+
+        if($("#role").length) {
+            $("#role").typed({
+                strings: roles,
+                typeSpeed: 50,
+                callback: () => obj.isRunning = false
+            });
+        }
     }
+    
 }
